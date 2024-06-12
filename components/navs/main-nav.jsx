89a37feb92c2a1cd179d8fc44ff6542c392dd832
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2024-06-11 21:04:12 -0500

    nav

---
+++ components/navs/main-nav.jsx
@@ -0,0 +1,22 @@
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    Transition,
+  } from '@headlessui/react'
+
+const NavMain = ({menuList })=>{
+    
+
+    return (<>
+        <Disclosure as="nav" className="bg-gray-800">
+            
+        </Disclosure>
+    </>)
+}
+
+export default NavMain
